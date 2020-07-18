8e53b7b973f07cb15b36b94180993a033ce2f47b
Lokathor <zefria@gmail.com>
2020-07-17 20:39:30 -0600

    try to be 1.32 compatible

--- src/lib.rs
+++ src/lib.rs
@@ -3002,13 +3002,13 @@ fn load_dyn_name_atomic_ptr(
   // if this fails the code generator itself royally screwed up somehow,
   // and so it's only a debug assert.
   debug_assert_eq!(*fn_name.last().unwrap(), 0);
-  let p: *mut c_void = get_proc_address(fn_name.as_ptr().cast());
+  let p: *mut c_void = get_proc_address(fn_name.as_ptr() as *const c_char);
   let p_usize = p as usize;
   // You *should* get null for failed lookups, but some systems have been
   // reported to give "error code" values such as -1 or small non-null values.
   // To help guard against this silliness, we consider these values to also
   // just be a result of null.
-  if p_usize == usize::MAX || p_usize < 8 {
+  if p_usize == core::usize::MAX || p_usize < 8 {
     ptr.store(null_mut(), RELAX);
     false
   } else {
